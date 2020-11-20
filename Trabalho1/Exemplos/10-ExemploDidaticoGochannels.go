d0c820336796e09b511770b181a0a01bfa36e0fe
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2020-11-20 16:41:49 -0300

    Update 10-ExemploDidaticoGochannels.go

--- Trabalho1/Exemplos/10-ExemploDidaticoGochannels.go
+++ Trabalho1/Exemplos/10-ExemploDidaticoGochannels.go
@@ -16,3 +16,6 @@ func main() {
     go say("world", done)
     fmt.Println(<-done)
 }
+
+//Aqui a thread principal aguarda uma mensagem da função say() para continuar
+//função essa que está sendo executada na própria goroutine.
