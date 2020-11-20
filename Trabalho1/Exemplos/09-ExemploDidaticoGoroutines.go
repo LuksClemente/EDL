f9cbcd3d60f5f6e0edef5210b1868b2a444c13fc
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2020-11-20 16:41:09 -0300

    Update 09-ExemploDidaticoGoroutines.go

--- Trabalho1/Exemplos/09-ExemploDidaticoGoroutines.go
+++ Trabalho1/Exemplos/09-ExemploDidaticoGoroutines.go
@@ -13,3 +13,7 @@ func main() {
     go say("world")
     say("hello")
 }
+
+//Aqui se fizéssemos a  chamada da função sem a goroutine: say(“hello”), nada seria impresso
+//já que essa rotina é independente da thread principal
+//sendo assim, ela terminaria antes que houvesse o print dos valores.
